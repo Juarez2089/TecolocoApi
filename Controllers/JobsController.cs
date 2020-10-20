290fee75c86d30638c9105d0ff506d4cd850514b
m j <mmejia2089@hotmail.com>
2020-10-19 21:19:41 -0600

    AddingGetJobs

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TecolocoApi.Models;
 
 namespace TecolocoApi.Controllers
 {
     public class JobsController : ApiController
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "GUTDAp1soq90dwqPDnNmnsiBPmD21ZLeCEHJqhzL",
+            BasePath = "https://tecoapp-b11a1.firebaseio.com/"
+        };
+        IFirebaseClient cliente;
+        [HttpGet]
+        public IEnumerable<Jobs> GetJobs()
+        {
+            cliente = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = cliente.Get("Jobs");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Jobs>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<Jobs>(((JProperty)item).Value.ToString()));
+                }
+            }
+
+            return list;
+        }
     }
 }
---
+++ Models/Jobs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TecolocoApi.Models
+{
+    public class Jobs
+    {
+        public string JobID { get; set; }
+        public string JobName { get; set; }
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
